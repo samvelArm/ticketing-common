6d2de9c48bf3f3e53ba25392889047b027ac9c0b
Samvel <samvel.antanyan@gmail.com>
2025-10-16 12:36:23 +0400

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -10,7 +10,7 @@ export abstract class Listener<T extends Event> {
   abstract subject: T['subject'];
   abstract onMessage(data: T['data'], msg: Message): void;
   abstract queueGroupName: string;
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
--- src/events/tickets/ticket-types.ts
+++ src/events/tickets/ticket-types.ts
@@ -4,4 +4,5 @@ export interface Ticket {
   price: number;
   userId: string;
   version: number;
+  orderId?: string;
 }
